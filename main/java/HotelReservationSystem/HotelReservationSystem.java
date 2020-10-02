782cc6735808acbd3f7d0f231a7c309352576bbb
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-10-02 19:30:49 +0530

    Add master

---
+++ main/java/HotelReservationSystem/HotelReservationSystem.java
@@ -0,0 +1,8 @@
+package HotelReservationSystem;
+
+public class HotelReservationSystem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservartion Program");
+	}
+}
